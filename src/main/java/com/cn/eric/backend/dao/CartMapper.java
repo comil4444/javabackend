211d336ddf02e0fb8a002a970e00b17af8ec4ca7
Cui, Eric <eric.cui@sap.com>
2018-05-06 20:31:19 +0800

    init project

---
+++ src/main/java/com/cn/eric/backend/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.cn.eric.backend.dao;
+
+import com.cn.eric.backend.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/cn/eric/backend/dao/CategoryMapper.java
@@ -0,0 +1,17 @@
+package com.cn.eric.backend.dao;
+
+import com.cn.eric.backend.pojo.Category;
+
+public interface CategoryMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    Category selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
+}
